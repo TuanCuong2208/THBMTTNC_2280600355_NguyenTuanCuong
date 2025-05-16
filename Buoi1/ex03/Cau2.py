c2559393139328e572c79a71e1e9bae400fde3d3
TuanCuong2208 <nguyentuancuong626tp@gmail.com>
2025-05-16 11:16:29 +0700

    Lab01

---
+++ Buoi1/ex03/Cau2.py
@@ -0,0 +1,5 @@
+def dao_nguoc_list(lst):
+    return lst[::-1]
+input_list = input("Nhập danh sách các số, cách nhau bằng dấu phẩy: ")
+numbers = list(map(int, input_list.split(',')))
+print("List sau khi đảo ngược:", dao_nguoc_list(numbers))
