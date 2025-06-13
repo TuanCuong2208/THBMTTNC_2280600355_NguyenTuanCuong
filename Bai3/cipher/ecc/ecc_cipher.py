37005ea113464178f46ce23770c268fbd88da657
TuanCuong2208 <nguyentuancuong626tp@gmail.com>
2025-06-13 11:21:40 +0700

    Add files via upload

---
+++ Bai3/cipher/ecc/ecc_cipher.py
@@ -0,0 +1,38 @@
+import ecdsa, os
+
+if not os.path.exists('cipher/ecc/keys'):
+    os.makedirs('cipher/ecc/keys')
+
+class ECCCipher:
+    def __init__(self):
+        pass
+
+    def generate_keys(self):
+        # Tạo khóa riêng và khóa công khai
+        sk = ecdsa.SigningKey.generate()
+        vk = sk.get_verifying_key()
+        # Lưu khóa riêng và khóa công khai
+        with open('cipher/ecc/keys/private.pem', 'wb') as p:
+            p.write(sk.to_pem())
+        with open('cipher/ecc/keys/public.pem', 'wb') as p:
+            p.write(vk.to_pem())
+        return sk, vk
+
+    def load_keys(self):
+        with open('cipher/ecc/keys/private.pem', 'rb') as p:
+            sk = ecdsa.SigningKey.from_pem(p.read())
+        with open('cipher/ecc/keys/public.pem', 'rb') as p:
+            vk = ecdsa.VerifyingKey.from_pem(p.read())
+        return sk, vk
+
+    def sign(self, message, key):
+        # Mã hóa tin nhắn bằng khóa riêng
+        return key.sign(message.encode('ascii'))
+
+    def verify(self, message, signature, key):
+        _, vk = self.load_keys()
+        try:
+            # Xác minh tin nhắn bằng khóa công khai
+            return vk.verify(signature, message.encode('ascii'))
+        except ecdsa.BadSignatureError:
+            return False
